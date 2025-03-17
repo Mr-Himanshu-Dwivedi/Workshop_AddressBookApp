45606592b075a950d12f9d593f60fd4132ada845
Mr-Himanshu-Dwivedi <himanshudwivedi2003@gmail.com>
2025-03-17 16:44:22 +0530

    ModelMapper and DTO

--- src/main/java/com/app/AddressBook_Workshop/service/IAddressBookService.java
+++ src/main/java/com/app/AddressBook_Workshop/service/IAddressBookService.java
@@ -1,14 +1,15 @@
 package com.app.AddressBook_Workshop.service;
 
+import com.app.AddressBook_Workshop.dto.AddressBookDTO;
 import com.app.AddressBook_Workshop.model.AddressBookEntry;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface IAddressBookService {
-    List<AddressBookEntry> getAllContacts();
-    Optional<AddressBookEntry> getContactById(Long id);
-    AddressBookEntry addContact(AddressBookEntry contact);
-    AddressBookEntry updateContact(Long id, AddressBookEntry updatedContact);
+    List<AddressBookDTO> getAllContacts();
+    Optional<AddressBookDTO> getContactById(Long id);
+    AddressBookDTO addContact(AddressBookDTO contactDTO);
+    AddressBookDTO updateContact(Long id, AddressBookDTO contactDTO);
     void deleteContact(Long id);
 }
